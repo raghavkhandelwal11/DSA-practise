86f0d122bfc9e3f9fe0e3124724341a398d90282
Raghav Khandelwal <gegerot@Raghavs-MacBook-Air.local>
2026-04-15 04:43:36 +0530

    first

---
+++ python_basics.py
@@ -0,0 +1,24 @@
+#print('hello world');
+# name = input("Enter your name: ");
+# print(name);
+
+
+
+# string
+
+# to find the length of string
+str1 = 'new world';
+str2 = 'hello ';
+len1 = len(str1);
+# print(len1);
+# print (str1 + str2);
+
+
+# slicing of string
+
+str = "Apna College";
+#str[2] = 'a';  not possible 
+print(str[2]);
+
+print(str[2:7]);
+

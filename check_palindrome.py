bbfe299ab75109e13b853d9e594045d0912e45a8
Raghav Khandelwal <gegerot@Raghavs-MacBook-Air.local>
2026-04-16 08:51:47 +0530

    save

---
+++ check_palindrome.py
@@ -0,0 +1,20 @@
+# check palindrome for numbers
+
+def check_palindrome(n):
+    num = n
+    reversed_number = 0;
+    
+
+    while(num>0):
+        digit = num%10;
+        num = num // 10;
+        reversed_number = reversed_number*10 + digit;
+
+    if(reversed_number == n):
+        return "Number is a palindrome";
+
+    return "Number is not a palindrome";  
+
+
+print(check_palindrome(1231));
+
